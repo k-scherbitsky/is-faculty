f0e7dab5ccfe2442f4161c8149a9cd6c0561e706
Konstantin Scherbitsky <k.scherbitsky@gmail.com>
2019-11-18 01:05:32 +0300

    Initial commit

---
+++ src/main/kotlin/io/faculty/isfaculty/ISFacultyApplication.kt
@@ -0,0 +1,11 @@
+package io.faculty.isfaculty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ISFacultyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ISFacultyApplication>(*args)
+}
---
+++ src/test/kotlin/io/faculty/isfaculty/ISFacultyApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.faculty.isfaculty
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ISFacultyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
