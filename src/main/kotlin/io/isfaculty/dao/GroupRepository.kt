b98ff2253c483b1d3209562aad3d3641d59c6b3d
Konstantin Scherbitsky <k.scherbitsky@gmail.com>
2019-12-01 15:37:08 +0300

    Add menu for work with student

--- src/main/kotlin/io/isfaculty/dao/GroupRepository.kt
+++ src/main/kotlin/io/isfaculty/dao/GroupRepository.kt
@@ -2,9 +2,12 @@ package io.isfaculty.dao
 
 import io.isfaculty.model.GroupEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface GroupRepository: JpaRepository<GroupEntity, Long> {
 
-    fun findByName(name: String): GroupEntity
+    @Query("select g from GroupEntity g where lower(g.name)= lower(:name)")
+    fun findByName(@Param("name") name: String): GroupEntity
 
 }
